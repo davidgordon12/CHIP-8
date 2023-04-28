dd467e7bbafd1fa9db7d91c9aaba4ca350c64e65
tehtko <moderndsnsam@gmail.com>
2023-04-27 21:41:36 -0400

    Implement fetch

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,29 @@ const NUM_V_REG: usize        =  16;
 const NUM_KEYS: usize         =  16;
 const RAM_SIZE: usize         =  4096;
 const STACK_SIZE: usize       =  16;
-const FONTSET_SIZE: u16       =  80;
+const FONTSET_SIZE: usize     =  80;
 const MAX_PROGRAM_MEMORY: u16 =  3584;
 const PROGRAM_START_ADDR: u16 =  0x200;
 const PROGRAM_END_ADDR: u16   =  0xFFF;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0 
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0,
+    0x90, 0x90, 0xF0, 0x10, 0x10,
+    0xF0, 0x80, 0xF0, 0x10, 0xF0,
+    0xF0, 0x80, 0xF0, 0x90, 0xF0,
+    0xF0, 0x10, 0x20, 0x40, 0x40,
+    0xF0, 0x90, 0xF0, 0x90, 0xF0,
+    0xF0, 0x90, 0xF0, 0x10, 0xF0,
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0,
+    0xF0, 0x80, 0x80, 0x80, 0xF0,
+    0xE0, 0x90, 0x90, 0x90, 0xE0,
+    0xF0, 0x80, 0xF0, 0x80, 0xF0,
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
 
 pub struct Emulator {
     pc: u16,
@@ -27,7 +46,7 @@ pub struct Emulator {
 
 impl Emulator {
     fn new() -> Self {
-        Self {
+       let mut emu = Self {
             pc: PROGRAM_START_ADDR,
             ram: [0; RAM_SIZE],
             stack: [0; STACK_SIZE],
@@ -38,17 +57,73 @@ impl Emulator {
             sp: 0,
             dt: 0,
             st: 0,
+        };
+
+        emu.ram[0..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        emu
+    }
+
+    fn reset(&mut self) {
+        self.pc = PROGRAM_START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.stack = [0; STACK_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v = [0; NUM_V_REG];
+        self.index = 0;
+        self.sp = 0;
+        self.dt = 0;
+        self.st = 0;
+        self.keys = [false; NUM_KEYS];
+        self.ram[0..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let hi_byte = self.ram[self.pc as usize] as u16;
+        let lo_byte = self.ram[(self.pc + 1) as usize] as u16;
+
+        let op = (hi_byte << 8) | lo_byte;
+        self.pc += 2;
+
+        op
+    }
+
+    fn decode(&mut self, op: u16) -> &str {
+        "CLS"
+    }
+
+    fn execute(&mut self, op: u16) {
+       /* DECODE AND EXECUTE IN ONE FUNCTION TEMPORARILY */ 
+    }
+
+    fn cycle(&mut self) {
+        // Fetch -> Decode -> Execute
+        let op = self.fetch();
+
+        
+    }
+
+    fn increment_timers(&mut self) {
+        if self.dt > 0 { self.dt -= 1; }
+
+        if self.st > 0 {
+            if self.st == 1 {
+                // beep
+            }
+            self.st -= 1; 
         }
     }
 
     fn push(&mut self, value: u16) {
         if self.sp >= 16 { panic("Cannot push onto stack, pointer at 16") }
+
         self.stack[self.sp as usize] = value;
         self.sp += 1;
     }
 
     fn pop(&mut self) -> u16 {
         if self.sp <= 0 { panic("Cannot pop stack, pointer at 0.") }
+
         self.sp -= 1;
         self.stack[self.sp as usize]
     }
@@ -56,9 +131,10 @@ impl Emulator {
 
 fn panic(info: &str) -> ! {
     println!("Panic: {}", info);
-    loop{}
+    loop {}
 }
 
 fn main() {
     let mut emulator = Emulator::new();
+    emulator.reset();
 }
