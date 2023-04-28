faf0b8bdde518fbaad9444b994007325b0d36882
tehtko <moderndsnsam@gmail.com>
2023-04-28 19:07:35 -0400

    Decode all possible instructions

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,4 +1,3 @@
-
 #![allow(dead_code)]        // allowed for now
 #![allow(unused_mut)]       // allowed for now
 #![allow(unused_variables)] // allowed for now 
@@ -90,26 +89,82 @@ impl Emulator {
     }
 
     pub fn decode(&mut self, op: u16) -> &str {
-        match(op) { 
-            _ => self.panic("Unimplemented opcode received."),
+        let digit1 = (op & 0xF000) >> 12;
+        let digit2 = (op & 0x0F00) >> 8; 
+        let digit3 = (op & 0x00F0) >> 4;
+        let digit4 = op & 0x000F;
+        match(digit1, digit2, digit3, digit4) {
+            (0, 0, 0, 0) => return "0000",
+            (0, 0, 0xE, 0) => return "00E0",
+            (0, 0, 0xE, 0xE) => return "00E0",
+            (1, _, _, _) => return "1nnn",
+            (2, _, _, _) => return "2nnn",
+            (3, _, _, _) => return "3nnn",
+            (4, _, _, _) => return "4xnn",
+            (5, _, _, 0) => return "5xy0",
+            (6, _, _, _) => return "6xnn",
+            (7, _, _, _) => return "7xnn",
+            (8, _, _, 0) => return "8xy0",
+            (8, _, _, 1) => return "8xy1",
+            (8, _, _, 2) => return "8xy2",
+            (8, _, _, 3) => return "8xy3",
+            (8, _, _, 4) => return "8xy4",
+            (8, _, _, 5) => return "8xy5",
+            (8, _, _, 6) => return "8xy6",
+            (8, _, _, 7) => return "8xy7",
+            (8, 0, 0, 0xE) => return "8xyE",
+            (9, _, _, 0) => return "9xy0",
+            (0xA, _, _, _) => return "Annn",
+            (0xB, _, _, _) => return "Bnnn",
+            (0xC, _, _, _) => return "Cxnn",
+            (0xD, _, _, _) => return "Dxyn",
+            (0xE, _, 9, 0xE) => return "Ex93",
+            (0xE, _, 0xA, 1) => return "ExA1",
+            (0xF, _, 0, 7) => return "Fx07",
+            (0xF, _, 0, 0xA) => return "Fx0A",
+            (0xF, _, 1, 5) => return "Fx15",
+            (0xF, _, 1, 8) => return "Fx18",
+            (0xF, _, 1, 0xE) => return "Fx1E",
+            (0xF, _, 2, 9) => return "Fx29",
+            (0xF, _, 3, 3) => return "Fx33",
+            (0xF, _, 5, 5) => return "Fx55",
+            (0xF, _, 6, 5) => return "Fx65",
             
+            (_, _, _, _) => self.panic("Unimplemented opcode received."),
         }
     }
 
-    pub fn execute(&mut self, opcode: String) -> u8 {
-       /* DECODE AND EXECUTE IN ONE FUNCTION TEMPORARILY */ 
-        0
+    pub fn execute(&mut self, opcode: &str, op: u16) {
+        match opcode {
+            // NOP
+            "0000" => return,
+
+            // CLS
+            "00E0" =>  { 
+                self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT]; 
+            }
+
+            // RET
+            "00EE" => {
+                let addr = self.pop();
+                self.pc = addr;
+            }
+
+            // JMP
+
+            _ => self.panic("Unimplemented")
+        }
     }
 
     pub fn cycle(&mut self) {
         // Fetch -> Decode -> Execute
-        let opcode = self.fetch();
+        let op = self.fetch();
         
-        // Drop the reference by copying result to op
-        let op = self.decode(opcode).to_string();
+        // Drop the reference by copying result to opcode
+        let opcode = self.decode(op).to_string();
 
-        // Now we can reference *self again
-        self.execute(op); 
+        // Now we can reference *self again, pass opcode as a slice
+        self.execute(&opcode[..], op); 
     }
 
     pub fn increment_timers(&mut self) {
